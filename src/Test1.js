06a67bd757d455ef36fb40e52d92b9643e7b0c0c
officename <ankitrajput2730@gmail.com>
2023-03-14 15:06:06 +0530

    feat/hariomsir

---
+++ src/Test1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Test2} from './Test2';
+
+const Test1 = () => {
+  return (
+    <div>
+      <h1>Ankit Rajput 1</h1>
+      <Test2/>
+    </div>
+  )
+}
+
+export default Test1
---
+++ src/Test2.js
@@ -0,0 +1,6 @@
+export let Test2 = () => {
+  return (
+    <h2>Ankit Rajput 2</h2>
+  )
+}
+
